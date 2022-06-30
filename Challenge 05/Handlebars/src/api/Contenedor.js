355631e16e21095c5fe8ce8dac81177d18385420
Ivan Martin <ivan.martin@aspentech.com>
2022-06-29 23:40:41 -0500

    adding(challenge-05): ejs-pug-handlebar

---
+++ Challenge 05/Handlebars/src/api/Contenedor.js
@@ -0,0 +1,66 @@
+//se inicia un nuevo proyecto
+//en consola:
+//*************  npm init -y   *****  en la carpeta src
+//colocar: ****  "type":"module"   **** en el package.json
+
+
+class ContenedorMemoria {
+    constructor(){
+        this.elements = [
+            {
+                title: "Calculadora niños",
+                price: 875,
+                thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png"
+            },
+            {
+                title: "Camión volcador",
+                price: 1600,
+                thumbnail: "https://cdn3.iconfinder.com/data/icons/transportation-14/512/Transportation_unloading_truck-256.png"
+
+            }
+        ]
+    }
+
+    getAll() {
+        return this.elements;
+    }
+
+    getById(id) {
+        const element = this.elements.find((e)=> e.id == id);
+
+        return element;
+    }
+
+    save(element){
+        element.id = this.elements.length === 0 ? 1 : this.elements[this.elements.length - 1].id + 1
+
+        this.elements.push(element);
+
+        return element;
+    }
+
+    updateById (id,newData){
+        const elementIndex = this.elements.findIndex((e)=> e.id ==id)
+
+        if(elementIndex === -1) return {error:true}
+
+        this.elements[elementIndex] = {
+            ...this.elements[elementIndex],
+            ...newData
+        };
+        return this.elements[elementIndex];
+    }
+
+    deleteById(id){
+        const elementIndex = this.elements.findIndex((e)=> e.id ==id)
+
+        if(elementIndex === -1) return {error:true}
+
+        this.elements = this.elements.filter((e)=> e.id != id);
+
+        return {error:false};
+    }
+
+}
+
+export {ContenedorMemoria};
---
+++ Challenge 05/README.md
@@ -0,0 +1,14 @@
+# Para correr el presente proyecto
+
+Basta con localizarse dentro de la carpeta Ejs, Pug o Handlebars y ejecutar las siguientes sentencias de código:
+
+Para instalar la paquetería
+>npm install
+
+Para inicializar el proyecto
+>npm start
+
+En cada una de las carpetas para desplegar los proyectos respectivos
+
+# En cuanto al uso de plantillas
+A futuro se piensa hacer uso de **EJS** debido a su practicidad y comodidad en las plantillas
